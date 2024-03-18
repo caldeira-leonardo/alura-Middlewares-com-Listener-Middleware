1d3534baa467275d60ae5a789e14157a72aa9665
Leonardo Caldeira <leob.caldeira@gmail.com>
2024-03-18 09:06:42 -0300

    remoção de itens não utilizados

--- src/store/reducers/categorias.js
+++ src/store/reducers/categorias.js
@@ -1,72 +1,60 @@
-import { createStandaloneToast } from '@chakra-ui/toast';
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import categoriasService from 'services/categorias';
-import { resetarCarrinho } from './carrinho';
+import { createStandaloneToast } from "@chakra-ui/toast";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import categoriasService from "services/categorias";
+import { resetarCarrinho } from "./carrinho";
 
 const { toast } = createStandaloneToast();
 
 const initialState = [];
 
 export const buscarCategorias = createAsyncThunk(
-  'categorias/buscar',
+  "categorias/buscar",
   categoriasService.buscar
 );
 
 const categoriasSlice = createSlice({
-  name: 'categorias',
+  name: "categorias",
   initialState,
-  extraReducers: builder => {
+  extraReducers: (builder) => {
     builder
-    .addCase(
-      buscarCategorias.fulfilled,
-      (state, { payload }) => {
+      .addCase(buscarCategorias.fulfilled, (_, { payload }) => {
         toast({
-          title: 'Sucesso!',
-          description: 'Categorias carregadas com sucesso!',
-          status: 'success',
+          title: "Sucesso!",
+          description: "Categorias carregadas com sucesso!",
+          status: "success",
           duration: 2000,
-          isClosable: true
-        })
+          isClosable: true,
+        });
         return payload;
-      }
-    )
-    .addCase(
-      buscarCategorias.pending,
-      (state, { payload }) => {
+      })
+      .addCase(buscarCategorias.pending, () => {
         toast({
-          title: 'Carregando',
-          description: 'Carregando categorias',
-          status: 'loading',
+          title: "Carregando",
+          description: "Carregando categorias",
+          status: "loading",
           duration: 2000,
-          isClosable: true
-        })
-      }
-    )
-    .addCase(
-      buscarCategorias.rejected,
-      (state, { payload }) => {
+          isClosable: true,
+        });
+      })
+      .addCase(buscarCategorias.rejected, () => {
         toast({
-          title: 'Erro',
-          description: 'Erro na busca de categorias',
-          status: 'error',
+          title: "Erro",
+          description: "Erro na busca de categorias",
+          status: "error",
           duration: 2000,
-          isClosable: true
-        })
-      }
-    )
-    .addCase(
-      resetarCarrinho.type,
-      () => {
+          isClosable: true,
+        });
+      })
+      .addCase(resetarCarrinho.type, () => {
         toast({
-          title: 'Sucesso!',
-          description: 'Compra completada com sucesso!',
-          status: 'success',
+          title: "Sucesso!",
+          description: "Compra completada com sucesso!",
+          status: "success",
           duration: 2000,
-          isClosable: true
-        })
-      }
-    )
-  }
+          isClosable: true,
+        });
+      });
+  },
 });
 
 export default categoriasSlice.reducer;
